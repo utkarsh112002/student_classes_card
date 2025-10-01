909fdc5ad320db32e567c1cc44ce75fa7e8d89b9
Utkarsh Sharma <usharma@academian.com>
2025-10-01 14:05:36 +0530

    file name changed

--- student_classes/extension.js
+++ student_classes/extension.js
@@ -1,82 +1,71 @@
 module.exports = {
-  name: "StudentClasses",
-  publisher: "Sample",
-  cards: [
-    {
-      type: "StudentClassesCard",
-      source: "./src/cards/StudentClassesCard",
-      title: "StudentClasses Card",
-      displayCardType: "StudentClasses Card",
-      description:
-        "This is an introductory card to the Ellucian Experience SDK",
-      queries: {
-        "section-registrations": [
-          {
-            resourceVersions: {
-              sectionRegistrations: { min: 6 },
-            },
-            query: `query registrantSectionsAndAcademicPeriods(registrantId: ID) {
-            sectionRegistration : sectionRegistrations16(
-      filter: { registrant12: { id: { EQ: $registrantId } } }
-    ) {
-      edges {
-        node {
-          id
-          registrant12 {
-            id
-          }
-          credit {
-            measure
-            registrationCredit
-          }
-          status {
-            registrationStatus
-          }
-          section16 {
-            id
-            titles {
-              value
-              type1 {
+  name: "StudentClassesAcademian",
+  publisher: "Utkarsh",
+  queries: {
+    "section-registrations": [
+      {
+        resourceVersions: {
+          sectionRegistrations: { min: 16 },
+          sections: { min: 16 },
+          courses: { min: 16 },
+          subjects: { min: 6 },
+          persons: { min: 12 },
+        },
+        query: `
+        query registrantSectionsAndAcademicPeriods($personId: ID) {
+          sectionRegistration: sectionRegistrations16(
+            filter: { registrant12: { id: { EQ: $personId } } }
+          ) {
+            edges {
+              node {
                 id
-                title
-                code
-              }
-            }
-            descriptions {
-              value
-            }
-            code
-            number
-            startOn
-            endOn
-            reportingAcademicPeriod16 {
-              id
-              code
-              title
-              startOn
-              endOn
-            }
-            course16 {
-              id
-              number
-              titles {
-                value
-              }
-              subject6 {
-                id
-                abbreviation
-                title
+                registrant12 { id }
+                credit {
+                  measure
+                  registrationCredit
+                }
+                status { registrationStatus }
+                section16 {
+                  id
+                  titles {
+                    value
+                  }
+                  descriptions { value }
+                  code
+                  number
+                  startOn
+                  endOn
+                  reportingAcademicPeriod16 {
+                    id
+                    code
+                    title
+                    startOn
+                    endOn
+                  }
+                  course16 {
+                    id
+                    number
+                    titles { value }
+                    subject6 { id abbreviation title }
+                  }
+                }
               }
             }
           }
         }
-      }
-    }
-  }
-`,
-          },
-        ],
+      `,
       },
+    ],
+  },
+  cards: [
+    {
+      type: "StudentClassesAcademianCard",
+      source: "./src/cards/StudentClassesAcademianCard",
+      title: "StudentClassesAcademian Card",
+      displayCardType: "StudentClassesAcaedemian Card",
+      description:
+        "This is an introductory card to the Ellucian Experience SDK",
+ 
       // pageRoute: {
       //     route: '/',
       //     excludeClickSelectors: ['a']
